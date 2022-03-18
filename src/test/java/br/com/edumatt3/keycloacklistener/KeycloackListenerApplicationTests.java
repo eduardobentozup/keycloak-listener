1478a2b1cc0aeb19d15306017d1cbc181425eac3
eduardobentozup <eduardo.bento@zup.com.br>
2022-03-18 13:26:16 -0300

    add spring project

---
+++ src/test/java/br/com/edumatt3/keycloacklistener/KeycloackListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edumatt3.keycloacklistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
